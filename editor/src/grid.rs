72ef31166a1e33937f954033f84a1197baea6c16
Sebastien Bedard <sebedard13@gmail.com>
2023-08-09 18:28:31 -0400

    feat: show coords from vgc

--- editor/src/grid.rs
+++ editor/src/grid.rs
@@ -1,7 +1,9 @@
 use iced::alignment;
 use iced::keyboard;
 use iced::mouse;
+use iced::mouse::Cursor;
 use iced::widget::canvas;
+use iced::widget::canvas::Fill;
 use iced::widget::canvas::event::{self, Event};
 use iced::widget::canvas::{Cache, Canvas, Frame, Geometry, Path, Text};
 use iced::{Color, Element, Length, Point, Rectangle, Renderer, Size, Theme, Vector};
@@ -43,6 +45,7 @@ impl Grid {
     const WIDTH: f32 = 500.0;
 
     pub fn update(&mut self, message: MsgGrid) {
+       
         match message {
             MsgGrid::Translated(translation) => {
                 self.translation = translation;
@@ -106,15 +109,18 @@ impl canvas::Program<MsgGrid> for Grid {
 
         let cursor_position = if let Some(position) = cursor.position_in(bounds) {
             position
+           
         } else {
             return (event::Status::Ignored, None);
         };
+
+      
 
         match event {
             Event::Mouse(mouse_event) => match mouse_event {
                 mouse::Event::ButtonPressed(button) => {
                     let message = match button {
-                        mouse::Button::Left => {
+                        mouse::Button::Right => {
                             *interaction = Interaction::Panning {
                                 translation: self.translation,
                                 start: cursor_position,
@@ -241,7 +247,6 @@ impl canvas::Program<MsgGrid> for Grid {
                 frame.fill_rectangle(Point::new(0 as f32, 0 as f32), size, color);
 
                 self.vgc_data.frame_render(frame, Self::WIDTH as u32);
-
             });
         });
 
@@ -260,14 +265,50 @@ impl canvas::Program<MsgGrid> for Grid {
             };
 
             if let Some(pos) = cursor_pos {
+              
                 let pos = self.project(pos, bounds.size());
+
+                let content = format!("({:.4}, {:.4}) {:.0}%", pos.x, pos.y, self.scaling * 100.0);
+
+                let overlay_width = content.len()as f32*6.58;
+                let overlay_height = 16.0;
+                
+                frame.fill_rectangle(text.position - Vector::new(overlay_width, overlay_height), 
+                Size::new(overlay_width, overlay_height), 
+                Fill::from(Color::from_rgba8(0x00, 0x00, 0x00, 0.8)));
+
+              
                 frame.fill_text(Text {
-                    content: format!("({:.4}, {:.4}) {:.0}%", pos.x, pos.y, self.scaling * 100.0),
+                    content,
                     position: text.position - Vector::new(0.0, 0.0),
                     ..text
                 });
             }
 
+            frame.with_save(|frame| {
+                frame.translate(center);
+                frame.scale(self.scaling);
+                frame.translate(self.translation);
+
+             // Render points
+             let coords = self.vgc_data.list_coord();
+             for coord in coords {
+                 let color = match cursor.position_in(bounds) {
+                     Some(p) => {
+                         if point_in_radius(&self.project(p, bounds.size()), 
+                         &Point::new(coord.coord.x, coord.coord.y), (12.0/self.scaling)/500.0) {
+                             Color::from_rgb8(0x0E, 0x90, 0xAA)
+                         }else{
+                             Color::from_rgb8(0x3A, 0xD1, 0xEF)
+                         }
+                     },
+                     None => Color::from_rgb8(0x3A, 0xD1, 0xEF)
+                 };
+                  
+                 let center = Point::new(coord.coord.x*Self::WIDTH, coord.coord.y*(Self::WIDTH / self.vgc_data.ratio as f32));
+                 frame.fill(&Path::circle(center, 5.0/self.scaling), Fill::from(color));
+             }
+            });
             frame.into_geometry()
         };
 
@@ -304,4 +345,29 @@ impl Default for Interaction {
     fn default() -> Self {
         Self::None
     }
+}
+
+
+/// Return true if the cursor is in the radius of the center
+///```rust
+///
+/// let cursor = Cursor::Available(Point::new(10.0, 10.0));
+/// let center = Point::new(0.0, 0.0);
+/// let radius = 5.0;
+/// assert_eq!(position_in_radius(cursor, center, radius), false);
+/// let cursor = Cursor::Available(Point::new(-3.0, 0.0));
+/// assert_eq!(position_in_radius(cursor, center, radius), true);
+///```
+pub fn position_in_radius(cursor:&Cursor, center:&Point, radius:f32) -> bool {
+    cursor.position().filter(|p| {
+       point_in_radius(p, center, radius)
+    }).is_some()
+}
+
+
+pub fn point_in_radius(point:&Point, center:&Point, radius:f32) -> bool {
+    let x = point.x - center.x;
+    let y = point.y - center.y;
+    let distance = x*x + y*y;
+    distance < (radius*radius)
 }
