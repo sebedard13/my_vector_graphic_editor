2bbaebca0caacfc06e216b79cb001340d27cc93e
Sébastien Bédard <sebedard13@gmail.com>
2023-08-31 16:11:40 -0400

    save: continue menubar

--- editor/src/toolbars/top.rs
+++ editor/src/toolbars/top.rs
@@ -11,12 +11,11 @@ use crate::{scene::Functionality, Message, VgcEditor};
 
 pub fn top_toolbar<'a>() -> Element<'a, Message, iced::Renderer> {
     let mb: iced_aw::menu::menu_bar::MenuBar<'_, Message> = menu_bar!(menu_1())
-        .item_width(ItemWidth::Uniform(180))
-        .item_height(ItemHeight::Uniform(27))
-        .spacing(4.0)
+        // .item_width(ItemWidth::Uniform(100))
+        // .item_height(ItemHeight::Uniform(20))
         .bounds_expand(30)
-        .main_offset(13)
-        .cross_offset(16)
+        .main_offset(4)
+        .cross_offset(10)
         .path_highlight(Some(PathHighlight::MenuActive))
         .close_condition(CloseCondition {
             leave: true,
@@ -27,7 +26,6 @@ pub fn top_toolbar<'a>() -> Element<'a, Message, iced::Renderer> {
     row!(mb, horizontal_space(Length::Fill))
         .padding([2, 8])
         .align_items(alignment::Alignment::Center)
-        .height(Length::Fixed(100.0))
         .into()
 }
 
@@ -36,10 +34,10 @@ fn menu_1<'a>() -> MenuTree<'a, Message, iced::Renderer> {
         labeled_button("File", Message::None),
         vec![
             menu_tree!(
-                labeled_button("Save", Message::None)
+                labeled_button("Save", Message::None).width(Length::Fill)
             ),
             menu_tree!(
-                labeled_button("Load", Message::None)
+                labeled_button("Load", Message::None).width(Length::Fill)
             )
         ],
     )
@@ -48,9 +46,10 @@ fn menu_1<'a>() -> MenuTree<'a, Message, iced::Renderer> {
 fn labeled_button<'a>(label: &str, msg: Message) -> button::Button<'a, Message, iced::Renderer> {
     base_button(
         text(label)
+            .size(12.0)
             .width(Length::Fill)
-            .height(Length::Fill)
-            .vertical_alignment(alignment::Vertical::Center),
+            .vertical_alignment(alignment::Vertical::Center)
+            .horizontal_alignment(alignment::Horizontal::Left),
         msg,
     )
 }
@@ -59,5 +58,6 @@ fn base_button<'a>(
     content: impl Into<Element<'a, Message, iced::Renderer>>,
     msg: Message,
 ) -> button::Button<'a, Message, iced::Renderer> {
-    button(content).padding([4, 8]).on_press(msg)
+    button(content)
+    .on_press(msg)
 }
