85450bec743f1fcaa8b5bc10fe8aab2842a0f2f5
Sebastien Bedard <sebedard13@gmail.com>
2024-01-03 10:01:22 -0500

    add selection rgba[]

--- view/src/app/selection.service.ts
+++ view/src/app/selection.service.ts
@@ -1,12 +1,27 @@
 import { Injectable } from '@angular/core';
-import { Selected } from 'wasm-vgc';
+import { Observable, Subject } from 'rxjs';
+import { Rgba, Selected } from 'wasm-vgc';
+import { ScenesService } from './scenes.service';
+
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class SelectionService {
 
-  public selection: Selected = new Selected();
+    public selectedColor$: Subject<Rgba[]> = new Subject<Rgba[]>();
 
-  constructor() { }
+    public selection: Selected = new Selected();
+
+    public selectionHasChanged: Subject<void> = new Subject<void>();
+
+    constructor(private scenesService: ScenesService) {
+
+        this.selectionHasChanged.asObservable().subscribe(() => {
+            this.scenesService.currentScene$.subscribe((scene) => {
+                let selectedColors = this.selection.get_selected_colors(scene);
+                this.selectedColor$.next(selectedColors);
+            });
+        });
+    }
 }
