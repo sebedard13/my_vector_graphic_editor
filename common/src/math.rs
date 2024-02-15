fd3dab11e8ab29add3dde4c2c2a9228376e0978d
Sebastien Bedard <sebedard13@gmail.com>
2024-02-15 15:47:47 -0500

    Test fix

--- common/src/math.rs
+++ common/src/math.rs
@@ -2,13 +2,15 @@ use super::pures::Vec2;
 
 /// Return true if the cursor is in the radius of the center
 ///```rust
+/// use common::math::point_in_radius;
+/// use common::pures::Vec2;
 ///
-/// let cursor = Vec2::new(10.0, 10.0));
+/// let cursor = Vec2::new(10.0, 10.0);
 /// let center = Vec2::new(0.0, 0.0);
 /// let radius = 5.0;
-/// assert_eq!(point_in_radius(cursor, center, radius), false);
-/// let cursor = Cursor::Available(Point::new(-3.0, 0.0));
-/// assert_eq!(point_in_radius(cursor, center, radius), true);
+/// assert_eq!(point_in_radius(&cursor, &center, radius), false);
+/// let cursor = Vec2::new(-3.0, 0.0);
+/// assert_eq!(point_in_radius(&cursor, &center, radius), true);
 ///```
 pub fn point_in_radius(point: &Vec2, center: &Vec2, radius: f32) -> bool {
     let x = point.x - center.x;
