a8bb8216e105787dba4408054c62edf2a95d4aa9
Sebastien Bedard <sebedard13@gmail.com>
2024-05-12 09:10:35 -0400

    toolbar shortcut

--- view/src/app/layout/tools-bar/tools-bar.component.ts
+++ view/src/app/layout/tools-bar/tools-bar.component.ts
@@ -1,5 +1,16 @@
-import { AfterViewInit, ChangeDetectionStrategy, Component, OnDestroy } from "@angular/core";
-import { Button, toolsbarSvgBtn } from "../../interface/button";
+import {
+    AfterViewInit,
+    ChangeDetectionStrategy,
+    Component,
+    DestroyRef,
+    ElementRef,
+    OnDestroy,
+    inject,
+} from "@angular/core";
+import { Button, toolsbarSvgBtn } from "src/app/interface/buttons";
+import { EventsService } from "src/app/scene/events.service";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
+import { filter } from "rxjs";
 @Component({
     selector: "app-tools-bar",
     templateUrl: "./tools-bar.component.html",
@@ -10,9 +21,40 @@ export class ToolsBarComponent implements AfterViewInit, OnDestroy {
     buttons: Button[] = toolsbarSvgBtn();
     activeButton!: Button;
 
+    private readonly eventService = inject(EventsService);
+    private readonly destroyRef = inject(DestroyRef);
+    private readonly elementRef = inject(ElementRef);
+
+    private readonly mapKeybinds: Map<string, number> = new Map([
+        ["Digit1", 0],
+        ["Digit2", 1],
+        ["Digit3", 2],
+        ["Digit4", 3],
+        ["Digit5", 4],
+        ["Digit6", 5],
+        ["Digit7", 6],
+        ["Digit8", 7],
+        ["Digit9", 8],
+        ["Digit0", 9],
+    ]);
+
     ngAfterViewInit(): void {
-        this.activeButton = this.buttons[0];
+        const startIndex = 0;
+        this.activeButton = this.buttons[startIndex];
+        this.elementRef.nativeElement.querySelector("#btn-id-" + startIndex).click();
+
         this.activeButton.functionality?.activate();
+        this.eventService.keydown$
+            .pipe(
+                takeUntilDestroyed(this.destroyRef),
+                filter((event) => this.mapKeybinds.has(event.code)),
+            )
+            .subscribe((event) => {
+                const index = this.mapKeybinds.get(event.code);
+                if (index != undefined && index < this.buttons.length) {
+                    this.elementRef.nativeElement.querySelector("#btn-id-" + index).click();
+                }
+            });
     }
 
     onclick(button: Button) {
