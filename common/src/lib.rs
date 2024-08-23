0f6a6ac8aa07f9eb56a7f69fecb5aec721693fec
Sebastien Bedard <sebedard13@gmail.com>
2024-08-23 07:07:00 -0400

    fix a test

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -22,6 +22,6 @@ mod tests {
     #[test]
     fn test_dbg_str() {
         let b = dbg_str!("test");
-        assert_eq!(b, "common\\src\\lib.rs:22: test");
+        assert_eq!(b, "common\\src\\lib.rs:24: test");
     }
 }
